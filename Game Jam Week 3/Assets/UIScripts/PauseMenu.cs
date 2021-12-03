9670c4638981ce0b8e04392570f6b2aae248fcf4
Jakk99 <JDArdon1999@outlook.com>
2021-12-02 20:47:02 -0800

    Adding in start of other menus(pause, win/lose conditions)

---
+++ Game Jam Week 3/Assets/UIScripts/PauseMenu.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+    public GameObject OptionsMenu;
+    public GameObject Confirmation;
+    public AudioSource OpenMenu;
+    public AudioSource CloseMenu;
+
+
+    public static bool GameisPaused = false;
+
+    void start()
+    {
+        Time.timeScale = 1f;
+        OptionsMenu.SetActive(false);
+        PauseMenuUI.SetActive(false);
+}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+        CloseMenu.Play();
+    }
+
+    void Pause()
+    {
+
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+        OpenMenu.Play();
+    }
+
+    public void OpenOptionsMenu()
+    {
+        if (OptionsMenu != null)
+        {
+            bool isActive = OptionsMenu.activeSelf;
+
+            OptionsMenu.SetActive(!isActive);
+        }
+    }
+
+    public void MainMenu()
+    {
+       // SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ConfirmQuit()
+    {
+        if (Confirmation != null)
+        {
+            bool isActive = Confirmation.activeSelf;
+
+            Confirmation.SetActive(!isActive);
+        }
+    }
+}
+
